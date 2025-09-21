9d250d001d00f28f89932fa19c6d7c5ee7cdfe3c
lesspr-ux <alesspmr@gmail.com>
2025-09-20 23:55:45 -0500

    agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // 1. Crear un array para almacenar los nombres
 let amigos = [];
+// 2. Función para agregar amigos
+function agregarAmigo() {
+  // Capturar el valor del campo de entrada
+  const input = document.getElementById("amigo");
+  let nombre = input.value.trim();
